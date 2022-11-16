4dd6d6bdab544e3b84c98c4c094795d6e2ce1315
NassimAlaimi <nassim.alaimi@epitech.eu>
2022-11-16 16:45:47 +0100

    [FIX]: indentation

--- react-website-smooth-scroll-yt/src/components/Signin/index.js
+++ react-website-smooth-scroll-yt/src/components/Signin/index.js
@@ -6,18 +6,18 @@ const SignIn = () => {
     <>
       <Container>
         <FormWrap>
-            <Icon to='/'>La Fabrique</Icon>
-            <FormContent>
-                <Form action='#'>
-                    <FormH1>Sign in to your account</FormH1>
-                    <FormLabel htmlFor='for'>Email</FormLabel>
-                    <FormInput type='email' required />
-                    <FormLabel htmlFor='for'>Password</FormLabel>
-                    <FormInput type='password' required />
-                    <FormButton type='submit'>Continue</FormButton>
-                    <Text>Forgot password</Text>
-                </Form>
-            </FormContent>
+          <Icon to='/'>La Fabrique</Icon>
+          <FormContent>
+            <Form action='#'>
+              <FormH1>Sign in to your account</FormH1>
+              <FormLabel htmlFor='for'>Email</FormLabel>
+              <FormInput type='email' required />
+              <FormLabel htmlFor='for'>Password</FormLabel>
+              <FormInput type='password' required />
+              <FormButton type='submit'>Continue</FormButton>
+              <Text>Forgot password</Text>
+            </Form>
+          </FormContent>
         </FormWrap>
       </Container>
     </>

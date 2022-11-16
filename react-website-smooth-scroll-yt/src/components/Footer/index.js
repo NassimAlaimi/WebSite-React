d14f3fc989578a7e0e31532eedc651ea9eb0da0f
NassimAlaimi <nassim.alaimi@epitech.eu>
2022-11-16 16:25:17 +0100

    [ADD]: SignIn Page

--- react-website-smooth-scroll-yt/src/components/Footer/index.js
+++ react-website-smooth-scroll-yt/src/components/Footer/index.js
@@ -1,8 +1,13 @@
 import React from 'react'
+import { animateScroll as scroll } from 'react-scroll'
 import { FooterContainer, FooterWrap, FooterLinksContainer, FooterLinksWrapper, FooterLinkItems, FooterLinkTitle, FooterLink, SocialMedia, SocialMediaWrap, SocialLogo, WebsiteRights, SocialIcons, SocialIconLink } from './FooterElement'
 import { FaFacebook, FaInstagram, FaYoutube, FaTwitter, FaLinkedin } from 'react-icons/fa'
 
 const Footer = () => {
+    const toggleHome = () => {
+        scroll.scrollToTop();
+    }
+
   return (
     <FooterContainer>
         <FooterWrap>
@@ -43,7 +48,7 @@ const Footer = () => {
             </FooterLinksContainer>
             <SocialMedia>
                 <SocialMediaWrap>
-                    <SocialLogo to='/'>
+                    <SocialLogo to='/' onClick={toggleHome}>
                         La Fabrique
                     </SocialLogo>
                     <WebsiteRights>La Fabrique © {new Date().getFullYear()} All rights reserved.</WebsiteRights>

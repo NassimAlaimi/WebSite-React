80c9d19bee08845caccfbe8c847fbdd7a44381b1
NassimAlaimi <nassim.alaimi@epitech.eu>
2022-11-16 15:41:17 +0100

    [ADD]: basic scrool navbar color

--- react-website-smooth-scroll-yt/src/components/Navbar/index.js
+++ react-website-smooth-scroll-yt/src/components/Navbar/index.js
@@ -1,35 +1,54 @@
-import React from 'react'
+import React, {useState} from 'react'
+import { useEffect } from 'react'
 import { FaBars } from 'react-icons/fa'
 import { Nav, NavbarContainer, NavLogo, MobileIcon, NavMenu, NavItem, NavLinks, NavBtn, NavBtnLink } from './NavbarElement'
+import { IconContext } from 'react-icons/lib'
+
 
 const Navbar = ({ toggle }) => {
+  const [scrollNav, setScrollNav] = useState(false)
+
+  const changeNav = () => {
+    if (window.scrollY >= 80) {
+      setScrollNav(true)
+    } else {
+      setScrollNav(false)
+    }
+  }
+
+  useEffect(() => {
+    window.addEventListener('scroll', changeNav)
+  }, [])
+
   return (
     <>
-      <Nav>
-          <NavbarContainer>
-            <NavLogo to="/">La Fabrique</NavLogo>
-            <MobileIcon onClick={toggle}>
-              <FaBars />
-            </MobileIcon>
-            <NavMenu>
-              <NavItem>
-                <NavLinks to="about">About</NavLinks>
-              </NavItem>
-              <NavItem>
-                <NavLinks to="discover">Discover</NavLinks>
-              </NavItem>
-              <NavItem>
-                <NavLinks to="services">Services</NavLinks>
-              </NavItem>
-              <NavItem>
-                <NavLinks to="signup">Sign Up</NavLinks>
-              </NavItem>
-            </NavMenu>
-            <NavBtn>
-              <NavBtnLink to="/signin">Sign In</NavBtnLink>
-            </NavBtn>
-          </NavbarContainer>
-      </Nav>
+    <IconContext.Provider value={{ color: '#f9f9f9' }}>
+        <Nav scrollNav={scrollNav}>
+            <NavbarContainer>
+              <NavLogo to="/">La Fabrique</NavLogo>
+              <MobileIcon onClick={toggle}>
+                <FaBars />
+              </MobileIcon>
+              <NavMenu>
+                <NavItem>
+                  <NavLinks to="about">About</NavLinks>
+                </NavItem>
+                <NavItem>
+                  <NavLinks to="discover">Discover</NavLinks>
+                </NavItem>
+                <NavItem>
+                  <NavLinks to="services">Services</NavLinks>
+                </NavItem>
+                <NavItem>
+                  <NavLinks to="signup">Sign Up</NavLinks>
+                </NavItem>
+              </NavMenu>
+              <NavBtn>
+                <NavBtnLink to="/signin">Sign In</NavBtnLink>
+              </NavBtn>
+            </NavbarContainer>
+        </Nav>
+      </IconContext.Provider>
     </>
   )
 }

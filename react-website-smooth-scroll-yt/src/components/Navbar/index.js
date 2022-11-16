61f9c9f74e10198727219faeeaaad2865962a11d
NassimAlaimi <nassim.alaimi@epitech.eu>
2022-11-16 16:13:01 +0100

    [ADD]: scroll on button

--- react-website-smooth-scroll-yt/src/components/Navbar/index.js
+++ react-website-smooth-scroll-yt/src/components/Navbar/index.js
@@ -3,6 +3,7 @@ import { useEffect } from 'react'
 import { FaBars } from 'react-icons/fa'
 import { Nav, NavbarContainer, NavLogo, MobileIcon, NavMenu, NavItem, NavLinks, NavBtn, NavBtnLink } from './NavbarElement'
 import { IconContext } from 'react-icons/lib'
+import { animateScroll as scroll } from 'react-scroll'
 
 
 const Navbar = ({ toggle }) => {
@@ -20,27 +21,31 @@ const Navbar = ({ toggle }) => {
     window.addEventListener('scroll', changeNav)
   }, [])
 
+  const toggleHome = () => {
+    scroll.scrollToTop();
+  }
+
   return (
     <>
     <IconContext.Provider value={{ color: '#f9f9f9' }}>
         <Nav scrollNav={scrollNav}>
             <NavbarContainer>
-              <NavLogo to="/">La Fabrique</NavLogo>
+              <NavLogo to="/" onClick={toggleHome}>La Fabrique</NavLogo>
               <MobileIcon onClick={toggle}>
                 <FaBars />
               </MobileIcon>
               <NavMenu>
                 <NavItem>
-                  <NavLinks to="about">About</NavLinks>
+                  <NavLinks to="about" smooth={true} duration={500} spy={true} exact='true' offset={-80}>About</NavLinks>
                 </NavItem>
                 <NavItem>
-                  <NavLinks to="discover">Discover</NavLinks>
+                  <NavLinks to="discover" smooth={true} duration={500} spy={true} exact='true' offset={-80}>Discover</NavLinks>
                 </NavItem>
                 <NavItem>
-                  <NavLinks to="services">Services</NavLinks>
+                  <NavLinks to="services" smooth={true} duration={500} spy={true} exact='true' offset={-80}>Services</NavLinks>
                 </NavItem>
                 <NavItem>
-                  <NavLinks to="signup">Sign Up</NavLinks>
+                  <NavLinks to="signup" smooth={true} duration={500} spy={true} exact='true' offset={-80}>Sign Up</NavLinks>
                 </NavItem>
               </NavMenu>
               <NavBtn>
